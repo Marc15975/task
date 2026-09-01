0c9d19c91404407f378b027355877cb7d03f036b
Marc15975 <juan77288987@gmail.com>
2026-09-01 01:59:44 -0400

    update: types library google

--- apps/client/src/app/api/auth/[...nextauth]/route.ts
+++ apps/client/src/app/api/auth/[...nextauth]/route.ts
@@ -16,9 +16,9 @@ const handler = NextAuth({
   ],
   callbacks: {
     async signIn({ account, profile }) {
-      if (account.provider === "google") {
+      /*if (account.provider === "google") {
         return profile.email_verified && profile.email.endsWith("@example.com");
-      }
+      }*/
       return true; // Do different verification for other providers that don't have `email_verified`
     },
   },
